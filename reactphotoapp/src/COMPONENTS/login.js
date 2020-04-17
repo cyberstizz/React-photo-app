87d5a384a6093fb00fdcf69a8f8c135831396c53
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-17 10:54:17 -0400

    created login component and centered it

---
+++ reactphotoapp/src/COMPONENTS/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+export default class Login extends React.Component{
+    constructor(props){
+    super(props)
+    }
+
+    render(){
+        return(
+            <div id="splash">
+
+            </div>
+        )
+    }
+}
